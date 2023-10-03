750939ade8dd81906cb629633abca2d7bfa153b3
Felipe Martin <4166437+phillipus85@users.noreply.github.com>
2023-10-03 07:32:06 -0500

    clean code in nodelist tests

--- Test/list/test_struct_listnode.py
+++ Test/list/test_struct_listnode.py
@@ -97,6 +97,7 @@ class TestNode(unittest.TestCase):
         """
         # getting the global variables
         dtype_lt = self.global_params.get("CHECK_TYPE_LT")
+
         # iterate over tglobal params and create single linked list node
         for key, dtype in zip(self.global_params.keys(), dtype_lt):
             # ignore 2 keys from the global params
@@ -105,17 +106,16 @@ class TestNode(unittest.TestCase):
                 # create a single linked list node with test_data
                 node = Node(test_data)
                 # assert for node info is not None
-                c1 = node.info == test_data
+                assert node.info == test_data
                 # assert for node info is the same type as test_data
-                c2 = isinstance(node.info, dtype)
-                # assert all 3 conditions are true
-                assert all([c1, c2])
+                assert isinstance(node.info, dtype)
 
     def test_get_element(self):
         """test_get_element test the retrieval of the list node information.
         """
         # getting the global variables
         dtype_lt = self.global_params.get("CHECK_TYPE_LT")
+
         # iterate over the global params list and create a node for each type
         for key, dtype in zip(self.global_params.keys(), dtype_lt):
             # ignore 2 keys from the global params
@@ -124,19 +124,18 @@ class TestNode(unittest.TestCase):
                 # create a single linked list node with test_data
                 node = Node(test_data)
                 # assert the node info is the test_data
-                c1 = node.info == test_data
+                assert node.info == test_data
                 # assert the data can be retrieved with the get_info()
-                c2 = node.get_info() == test_data
+                assert node.get_info() == test_data
                 # assert get_info() returns the same type as test_data
-                c3 = isinstance(node.get_info(), dtype)
-                # assert all 3 conditions are true
-                assert all([c1, c2, c3])
+                assert isinstance(node.get_info(), dtype)
 
     def test_set_element(self):
         """test_set_element test the update of the list node information.
         """
         # getting the global variables
         dtype_lt = self.global_params.get("CHECK_TYPE_LT")
+
         # iterate over the global params list and create a node for each type
         for key, dtype in zip(self.global_params.keys(), dtype_lt):
             # ignore 2 keys from the global params
@@ -147,12 +146,10 @@ class TestNode(unittest.TestCase):
                 # update the node info with test data using set_info()
                 node.set_info(test_data)
                 # assert the node info is test_data
-                c1 = node.info == test_data
+                assert node.info == test_data
                 # assert the info type is the same as test_data
-                c2 = isinstance(node.info, dtype)
-                c3 = isinstance(test_data, dtype)
-                # assert all 3 conditions are true
-                assert all([c1, c2, c3])
+                assert isinstance(node.info, dtype)
+                assert isinstance(test_data, dtype)
 
     def test_node_typerr(self):
         """test_node_typerr test the type error handling for invalid data types
@@ -165,6 +162,7 @@ class TestNode(unittest.TestCase):
         dtype_lt = self.global_params.get("CHECK_TYPE_LT")
         # global params keys
         param_keys = self.global_params.keys()
+
         # iterate over the type error list and create a node for each type
         for key, dtype, err in zip(param_keys, dtype_lt, type_err_lt):
             # ignore 2 keys from the global params
@@ -176,13 +174,11 @@ class TestNode(unittest.TestCase):
                     # induce the error by changing the node info type
                     node.set_info(err)
                 # assert the type error is raised
-                c1 = "Invalid data type" in str(excinfo.value)
+                assert "Invalid data type" in str(excinfo.value)
                 # assert the node info is the same type as test_data
-                c2 = isinstance(test_data, dtype)
+                assert isinstance(test_data, dtype)
                 # assert the node info is not the same type as err
-                c3 = dtype != err
-                # assert all 3 conditions are true
-                assert all([c1, c2, c3])
+                assert dtype != err
 
     def test_node_handle_error(self):
         """test_node_handle_error test the error handling ad raise in the
@@ -203,9 +199,9 @@ class TestNode(unittest.TestCase):
         #         with pytest.raises(TypeError) as excinfo:
         #             node = SingleNode(test_data)
         #             node.set_info(err)
-        #         c1 = "Invalid data type" in str(excinfo.value)
-        #         c2 = isinstance(test_data, dtype)
-        #         c3 = dtype != err
+        #         assert "Invalid data type" in str(excinfo.value)
+        #         assert isinstance(test_data, dtype)
+        #         assert dtype != err
         #         assert all([c1, c2, c3])
         pass
 
@@ -233,11 +229,9 @@ class TestSingleNode(unittest.TestCase):
         # create an empty single linked list node
         node = SingleNode()
         # assert for node information is None
-        c1 = (node.info is None)
+        assert (node.info is None)
         # assert for node _next and next() are None
-        c2 = (node.next() is None) and (node._next is None)
-        # assert both conditions are true
-        assert all([c1, c2])
+        assert (node.next() is None) and (node._next is None)
 
     def test_new_custom_single_node(self):
         """test_new_custom_node test the creation of a single linked list node
@@ -245,6 +239,7 @@ class TestSingleNode(unittest.TestCase):
         """
         # getting the global variables
         dtype_lt = self.global_params.get("CHECK_TYPE_LT")
+
         # iterate over tglobal params and create single linked list node
         for key, dtype in zip(self.global_params.keys(), dtype_lt):
             # ignore 2 keys from the global params
@@ -253,19 +248,16 @@ class TestSingleNode(unittest.TestCase):
                 # create a single linked list node with test_data
                 node = SingleNode(test_data)
                 # assert for node info is not None
-                c1 = node.info == test_data
+                assert node.info == test_data
                 # assert for node info is the same type as test_data
-                c2 = isinstance(node.info, dtype)
+                assert isinstance(node.info, dtype)
                 # assert the node _next and next() are None
-                c3 = (node.next() is None) and (node._next is None)
-                # assert all 3 conditions are true
-                assert all([c1, c2, c3])
+                assert (node.next() is None) and (node._next is None)
 
     def test_single_node_next(self):
         """test_node_next thes the node _next attribute and next() method in
             the single linked list node.
         """
-        # getting the global variables
         # iterate over the global params list and create a node for each type
         for key in self.global_params.keys():
             # ignore 2 keys from the global params
@@ -275,20 +267,16 @@ class TestSingleNode(unittest.TestCase):
                 node1 = SingleNode(test_data)
                 node2 = SingleNode(test_data)
                 # assert the node _next and next() are None in both nodes
-                c1 = (node1.next() is None) and (node1._next is None)
-                c2 = (node2.next() is None) and (node2._next is None)
-                assert all([c1, c2])
+                assert (node1.next() is None) and (node1._next is None)
+                assert (node2.next() is None) and (node2._next is None)
                 # concatenate the nodes
                 node1._next = node2
                 # assert the node _next and next() are not None in node1
-                c1 = (node1.next() is not None) and (node1._next is not None)
+                assert (node1.next() is not None) and (node1._next is not None)
                 # assert the node _next and next() are node2 in node1
-                c2 = (node1.next() is node2) and (node1._next is node2)
+                assert (node1.next() is node2) and (node1._next is node2)
                 # assert the node _next and next() are None in node2
-                c3 = (node2.next() is None) and (node2._next is None)
-                # assert all 3 conditions are true
-                assert all([c1, c2, c3])
-
+                assert (node2.next() is None) and (node2._next is None)
 
 class TestDoubleNode(unittest.TestCase):
     """TestDoubleNode test class for testing the DoubleNode class.
@@ -312,13 +300,11 @@ class TestDoubleNode(unittest.TestCase):
         # create an empty single linked list node
         node = DoubleNode()
         # assert for node information is None
-        c1 = node.info is None
+        assert node.info is None
         # assert for node _next and next() are None
-        c2 = (node.next() is None) and (node._next is None)
+        assert (node.next() is None) and (node._next is None)
         # assert for node _prev and prev() are None
-        c3 = (node.prev() is None) and (node._prev is None)
-        # assert all 3 conditions are true
-        assert all([c1, c2, c3])
+        assert (node.prev() is None) and (node._prev is None)
 
     def test_new_custom_double_node(self):
         """test_new_custom_node test the creation of a double linked list node
@@ -326,6 +312,7 @@ class TestDoubleNode(unittest.TestCase):
         """
         # getting the global variables
         dtype_lt = self.global_params.get("CHECK_TYPE_LT")
+
         # iterate over the global params and create a node for each type
         for key, dtype in zip(self.global_params.keys(), dtype_lt):
             # ignore 2 keys from the global params
@@ -334,15 +321,13 @@ class TestDoubleNode(unittest.TestCase):
                 # create a double linked list node with test_data
                 node = DoubleNode(test_data)
                 # assert for node info is the test_data
-                c1 = node.info == test_data
+                assert node.info == test_data
                 # assert for node info is the same type as test_data
-                c2 = isinstance(node.info, dtype)
+                assert isinstance(node.info, dtype)
                 # assert for node _next and next() are None
-                c3 = (node.next() is None) and (node._next is None)
+                assert (node.next() is None) and (node._next is None)
                 # assert for node _prev and prev() are None
-                c4 = (node.prev() is None) and (node._prev is None)
-                # assert all 4 conditions are true
-                assert all([c1, c2, c3, c4])
+                assert (node.prev() is None) and (node._prev is None)
 
     def test_double_node_refs(self):
         """test_node_ref test the node references in the double linked list
@@ -360,24 +345,21 @@ class TestDoubleNode(unittest.TestCase):
                 node2 = DoubleNode(test_data)
                 # DEFAULT REFERENCES
                 # assert the node _next and next() are None in both nodes
-                c1 = (node1.next() is None) and (node1._next is None)
-                c2 = (node2.next() is None) and (node2._next is None)
+                assert (node1.next() is None) and (node1._next is None)
+                assert (node2.next() is None) and (node2._next is None)
                 # assert the node _prev and prev() are None in both nodes
-                c3 = (node1.prev() is None) and (node1._prev is None)
-                c4 = (node2.prev() is None) and (node2._prev is None)
-                # assert all 4 conditions are true
-                assert all([c1, c2, c3, c4])
+                assert (node1.prev() is None) and (node1._prev is None)
+                assert (node2.prev() is None) and (node2._prev is None)
+
                 # CUSTOM REFERENCES
                 # concatenate the nodes
                 node1._next = node2
                 node2._prev = node1
                 # assert the node _next and next() are not None in node1
-                c1 = (node1.next() is not None) and (node1._next is not None)
+                assert (node1.next() is not None) and (node1._next is not None)
                 # assert the node _prev and prev() are not None in node2
-                c2 = (node2.prev() is not None) and (node2._prev is not None)
+                assert (node2.prev() is not None) and (node2._prev is not None)
                 # assert node1 next() and _next are node2
-                c3 = (node1.next() is node2) and (node1._next is node2)
+                assert (node1.next() is node2) and (node1._next is node2)
                 # assert node2 prev() and _prev are node1
-                c4 = (node2.prev() is node1) and (node2._prev is node1)
-                # assert all 4 conditions are true
-                assert all([c1, c2, c3, c4])
+                assert (node2.prev() is node1) and (node2._prev is node1)

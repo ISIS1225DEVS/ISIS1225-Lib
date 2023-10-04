42d91f346485aba687743129a638730d182f41a6
Felipe Martin <4166437+phillipus85@users.noreply.github.com>
2023-10-04 18:22:16 -0500

    basic import proof

---
+++ main.py
@@ -0,0 +1,46 @@
+﻿from DISClib.ADT.lists import List
+
+
+def cmp_test(a, b):
+    key = "testkey"
+    ka = a.get(key)
+    kb = b.get(key)
+    if ka < kb:
+        return -1
+    elif ka > kb:
+        return 1
+    else:
+        return 0
+
+
+# main del ejercicio
+if __name__ == "__main__":
+    test_list = List(implementation="ArrayList")
+    print(test_list)
+    print(type(test_list))
+
+    test_data = [1, 2, 3, 4, 5]
+
+    for i in test_data:
+        test_list.add_last(i)
+
+    print(test_list)
+
+    test_data = [
+        {"testkey": 1, "testvalue": "one"},
+        {"testkey": 2, "testvalue": "two"},
+        {"testkey": 3, "testvalue": "three"},
+        {"testkey": 4, "testvalue": "four"},
+        {"testkey": 5, "testvalue": "five"},
+    ]
+
+    test_list = List(implementation="ArrayList",
+                     elements=test_data,
+                     cmp_function=cmp_test)
+    print(test_list)
+    print(type(test_list))
+    print("iterating ADT List")
+    for d in test_list:
+        print(d)
+
+    print(test_list.get_last())

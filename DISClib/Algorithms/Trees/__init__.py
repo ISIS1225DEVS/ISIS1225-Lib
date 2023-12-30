12da9800e5f1ec91aa8dd3f8367efbbf79ccd67e
Felipe Martin <4166437+phillipus85@users.noreply.github.com>
2023-12-30 18:40:45 -0500

    Remove unnecessary code for importing DISCLib folder

--- DISClib/Algorithms/Trees/__init__.py
+++
@@ -1,8 +0,0 @@
-# impoting the path to the DISCLib folder
-import os
-import sys
-# config the path to the DISCLib folder
-# TODO this used to be in config.py
-file_path = os.path.join(os.path.dirname(__file__), '..', '..')
-file_dir = os.path.dirname(os.path.realpath('__file__'))
-sys.path.insert(0, os.path.abspath(file_path))
